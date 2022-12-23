c910bb8fb1b1788e74106345f394ad513983655a
yongZin <vzb001@gmail.com>
2022-12-23 16:34:04 +0900

    (freitag) 스크롤 이벤트 디렉토리명 변경 및 php관련 "data" 디렉토리 추가

---
+++ freitag/data/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="ko">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>DATA</title>
+</head>
+<body>
+    <?php 
+        $var = "hello";
+        $output = $var. "world21";
+
+        echo $output;
+    ?>
+</body>
+</html>
